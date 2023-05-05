91fcca84820fe1b0f247e67c127304dbafb38ddb
mako314 <119079347+mako314@users.noreply.github.com>
2023-05-04 21:39:38 -0400

    finish functions for dodger game

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+//1 write function to move dodger
+//2 add variables to grab dodger style left and increment +1
+//3 test to see if right key is being pressed, and do not let it go past 360px on the right
+function moveDodgerRight(){
+    const rightNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(rightNumbers,10);
+
+    if (left < 360){
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    else if (e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+  });
